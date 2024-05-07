40ac20c791a360774e6a2b605b0f48b6ec94ed2d
arunc13 <arunconline@gmail.com>
2024-05-07 09:12:30 +0530

    Intro

--- OpenCV/introduction.py
+++ OpenCV/introduction.py
@@ -1,8 +1,15 @@
 import cv2
 
 img = cv2.imread('/home/arun/Lum/OpenCV/gp1.jpg')
-print(img)
+img1 = cv2.imread('/home/arun/Lum/OpenCV/gp2.jpg')
+# print(img)
 
-cv2.imshow('image',img)
-cv2.waitKey(50000)
+gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+(thresh,bw_img) = cv2.threshold(gray_img,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+print(thresh)
+
+cv2.imshow('image',bw_img)
+cv2.imshow('image2',img1)
+cv2.waitKey()
 cv2.destroyAllWindows()
