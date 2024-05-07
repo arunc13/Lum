eb1c2c99dd96dc6dbd33d4905fe7b737cb7dffc7
arunc13 <arunconline@gmail.com>
2024-05-07 09:17:55 +0530

    intro

--- OpenCV/introduction.py
+++ OpenCV/introduction.py
@@ -9,6 +9,9 @@ gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 (thresh,bw_img) = cv2.threshold(gray_img,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 print(thresh)
 
+cv2.imwrite('gp1_gray.jpg',gray_img)
+cv2.imwrite('gp1_bw.jpg',bw_img)
+
 cv2.imshow('image',bw_img)
 cv2.imshow('image2',img1)
 cv2.waitKey()
